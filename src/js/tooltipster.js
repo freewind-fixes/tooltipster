11713059ada21eb04e6b717c2f01d9f234a1536c
freewind <nowindlee@gmail.com>
2019-01-18 14:57:18 +0800

    modify bodyContains function to avoid id checking to fix the "duplicated ids" issue

--- src/js/tooltipster.js
+++ src/js/tooltipster.js
@@ -3282,12 +3282,23 @@ function areEqual(a,b) {
  *
  * @param {object} $obj A jQuery-wrapped HTML element
  * @return {boolean}
- */
+ *
 function bodyContains($obj) {
 	var id = $obj.attr('id'),
 		el = id ? env.window.document.getElementById(id) : null;
 	// must also check that the element with the id is the one we want
 	return el ? el === $obj[0] : $.contains(env.window.document.body, $obj[0]);
+}
+*/
+
+/**
+ * No longer use `id` to check since some pages don't follow the unique-id rule.
+ *
+ * @param {object} $obj A jQuery-wrapped HTML element
+ * @return {boolean}
+ */
+function bodyContains($obj) {
+	return $.contains(env.window.document.body, $obj[0]);
 }
 
 // detect IE versions for dirty fixes
