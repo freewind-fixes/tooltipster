c3c424db7034421f49668aa71d93bfffc14e068c
Freewind <nowindlee@gmail.com>
2022-09-22 16:09:39 +0800

    fix

--- dist/js/tooltipster.main.js
+++ dist/js/tooltipster.main.js
@@ -5,18 +5,19 @@
  * MIT license
  */
 (function (root, factory) {
+  if (root === undefined && window !== undefined) root = window;
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
     define(["jquery"], function (a0) {
       return (factory(a0));
     });
-  } else if (typeof exports === 'object') {
+  } else if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
     module.exports = factory(require("jquery"));
   } else {
-    factory(jQuery);
+    factory(root["jQuery"]);
   }
 }(this, function ($) {
 
@@ -3304,12 +3305,23 @@ function areEqual(a,b) {
  *
  * @param {object} $obj A jQuery-wrapped HTML element
  * @return {boolean}
- */
+ *
 function bodyContains($obj) {
 	var id = $obj.attr('id'),
 		el = id ? env.window.document.getElementById(id) : null;
 	// must also check that the element with the id is the one we want
 	return el ? el === $obj[0] : $.contains(env.window.document.body, $obj[0]);
+}
+*/
+
+/**
+ * No longer use `id` to check since some pages don't follow the unique-id rule.
+ *
+ * @param {object} $obj A jQuery-wrapped HTML element
+ * @return {boolean}
+ */
+function bodyContains($obj) {
+	return $.contains(env.window.document.body, $obj[0]);
 }
 
 // detect IE versions for dirty fixes
